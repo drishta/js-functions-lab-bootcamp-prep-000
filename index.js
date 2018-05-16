f268ae256c84e88dad5b8c55d797dc5b5a61e81e
Austin Katzer <austin484@gmail.com>
2018-05-16 21:03:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ function happyHolidaysTo (name)
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo ()
+function happyHolidayTo (holiday, name)
+{
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown (holiday, days)
+{
+  return `It's ${days} days until ${holiday}!`
+}
